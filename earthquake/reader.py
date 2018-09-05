e6519a5e22ee4b5d8543d13fd48f7ca4caf3dd19
fcracker79 <fcracker79@gmail.com>
2018-09-05 09:47:40 +0200

    Fixed bug. Added samples. Added tools.

--- earthquake/reader.py
+++ earthquake/reader.py
@@ -1,4 +1,5 @@
 import abc
+import re
 import typing
 
 from earthquake.eq_item import EarthquakeItem
@@ -15,23 +16,29 @@ class FileBasedEarthquakeReader(EarthquakeReader, metaclass=abc.ABCMeta):
         self._filename = filename
 
     @abc.abstractmethod
-    def read_from_line(self, idx: int, line: str) -> EarthquakeItem:
+    def read_from_line(self, idx: int, line: str) -> typing.Optional[EarthquakeItem]:
         pass
 
     def read(self) -> typing.Iterator[EarthquakeItem]:
         with open(self._filename, 'rt') as f:
-            for i, l in f.readline():
+            for i, l in enumerate(f.readlines()):
                 cur_item = self.read_from_line(i, l)
                 if cur_item:
                     yield cur_item
 
 
 class TabbedFileEarthquakeReader(FileBasedEarthquakeReader):
+    _STR_PATTERN = '([^\t ]+)[\t ]+([^\t ]+)'
+    _PATTERN = re.compile(_STR_PATTERN)
+
     def __init__(self, filename: str, header_lines: int):
         super(TabbedFileEarthquakeReader, self).__init__(filename)
         self._header_lines = header_lines
 
-    def read_from_line(self, idx: int, line: str) -> EarthquakeItem:
+    def read_from_line(self, idx: int, line: str) -> typing.Optional[EarthquakeItem]:
         if idx < self._header_lines:
             return None
-        return EarthquakeItem(*map(float, line.split('\t')))
+        matcher = re.fullmatch(self._PATTERN, line)
+        if not matcher:
+            return None
+        return EarthquakeItem(float(matcher.group(2)), float(matcher.group(1)))
